51c538e3609e0d5df23e910eaee4555ce57966a1
iproduct <office@iproduct.org>
2016-02-11 13:15:15 +0200

    TicketController demo code added.

--- tickets/src/tickets/controller/TicketController.java
+++ tickets/src/tickets/controller/TicketController.java
@@ -1,5 +1,8 @@
 package tickets.controller;
 
+import java.util.Arrays;
+import java.util.Date;
+
 import tickets.exception.EntityAlreadyExistsException;
 import tickets.model.Event;
 import tickets.model.Ticket;
@@ -17,11 +20,35 @@ public class TicketController {
 					"Event already added with ID: " + event.getId());
 			}
 		}
+		Event[] result = new Event[events.length + 1];
+		for(int i = 0; i < events.length; i++){
+			result[i] = events[i];
+		}
+		result[result.length - 1] = event;
+		events = result;
+	}
+	
+	public Event[] getEvents(){
+		return events;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TicketController controller = new TicketController();
+		Event event = new Event(
+				"OpenFest", "Festival of open source technologies",
+				"Phylocharmony", new Date(), new Date(), 1500, 0);
+		Event event2 = new Event(
+				"New Movie", "Comedy",
+				"NDK", new Date(), new Date(), 800, 10);
+		try {
+			controller.addEvent(event);
+			controller.addEvent(event2);
+			for(Event e : controller.getEvents())
+				System.out.println(e);
+		} catch (EntityAlreadyExistsException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
