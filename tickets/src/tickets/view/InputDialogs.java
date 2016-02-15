ab7632f155ab81221e9345731b5df1c0ecc92ea2
iproduct <office@iproduct.org>
2016-02-15 12:31:53 +0200

    TicketsApp added to main package.

---
+++ tickets/src/tickets/view/InputDialogs.java
@@ -0,0 +1,17 @@
+package tickets.view;
+
+import tickets.TicketsApp;
+import tickets.model.Event;
+
+public class InputDialogs {
+	private TicketsApp application;
+
+	public InputDialogs(TicketsApp mainApp) {
+		application = mainApp;
+	}
+	
+	public Event inputEvent() {
+		return null;
+	}
+
+}
