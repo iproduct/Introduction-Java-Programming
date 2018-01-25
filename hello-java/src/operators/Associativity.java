ce873ee9ead1c958a426b1943ae23520a927f13e
Trayan Iliev <ilievt@gmail.com>
2018-01-25 13:07:11 +0200

    hexadecimal conversion added

---
+++ hello-java/src/operators/Associativity.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class Associativity {
+
+	public static void main(String[] args) {
+		int a = 3, b = 4, c = 5;
+		System.out.println("Result " + (a + b + c));
+	}
+
+}
--- hello-java/src/operators/DecimalToBinary.java
+++ hello-java/src/operators/DecimalToBinary.java
@@ -7,6 +7,36 @@ public class DecimalToBinary {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Input a number:");
+		int number = sc.nextInt();
+		System.out.println("Decimal: " + number);
+		
+		// Decimal to binary conversion
+		int quotient = number;
+		int	remainder;
+		String binaryResult = "";
+		
+		while(quotient != 0) {
+			remainder = quotient % 2;
+			quotient /= 2;
+			binaryResult = remainder + binaryResult;
+		}
+		System.out.println("Binary: " + binaryResult);
+
+		// Decimal to hexadecimal conversion
+		String hexadecimalResult = "";
+		
+		quotient = number;
+		while(quotient != 0) {
+			remainder = quotient % 16;
+			quotient /= 16; // quotient = quotient / 16;
+			char hexDigit = 
+				(char)(remainder < 10 ? 
+					  '0' + remainder 
+					: 'A' + (remainder - 10));
+			hexadecimalResult = hexDigit + hexadecimalResult;
+		}
+		
+		System.out.println("Hex: " + hexadecimalResult);
 
 	}
 
