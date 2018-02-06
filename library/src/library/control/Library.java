115af5177bbd5cc05d60236d0f88fe28a42cc158
Trayan Iliev <ilievt@gmail.com>
2018-02-06 09:03:09 +0200

    Author finding and adding implemented

--- library/src/library/control/Library.java
+++ library/src/library/control/Library.java
@@ -3,14 +3,19 @@ package library.control;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import library.model.Author;
 import library.model.Book;
 import library.model.Genre;
 
 public class Library {
-	public static final int MAX_BOOKS = 1000;
+	public static final int MAX_ENTITIES = 1000;
 
-	private Book[] books = new Book[1000];
+	private Book[] books = new Book[MAX_ENTITIES];
 	private int numBooks = 0;
+	
+	private Author[] authors = new Author[MAX_ENTITIES];
+	private int numAuthors = 0;
+	
 	private Scanner sc = new Scanner(System.in);
 
 	public boolean addBook(Book book) {
@@ -22,12 +27,24 @@ public class Library {
 		return Arrays.copyOf(books, numBooks);
 	}
 
+	public boolean addAuthor(Author author) {
+		authors[numAuthors++] = author;
+		return true;
+	}
+
+	public Author[] getAllAuthors() {
+		return Arrays.copyOf(authors, numAuthors);
+	}
+
 	public void inputNewBook() {
 		Book book = new Book();
 		System.out.print("Title: ");
 		book.setTitle(sc.nextLine());
 		System.out.print("Authors: ");
-		book.setAuthors(sc.nextLine());
+		String authorStr = sc.nextLine();
+		String[] names = authorStr.trim().split(" ");
+		Author author = addAndGetAuthor(new Author(names[0], names[names.length - 1]));
+		book.setAuthors(new Author[] {author});
 		
 		System.out.print("Year: ");
 		String inputStr;
@@ -46,7 +63,7 @@ public class Library {
 		book.setYear(year);
 		
 		System.out.print("Publisher: ");
-		book.setAuthors(sc.nextLine());
+		book.setPublisher(sc.nextLine());
 
 		System.out.print("Genre (");
 		for(Genre genre: Genre.values())
@@ -70,6 +87,26 @@ public class Library {
 		addBook(book);
 	}
 
+	public Author findAuthorByNames(String fName, String lName ) {
+		for(int i = 0; i < numAuthors; i++) {
+			if(authors[i].getFirstName().equals(fName) 
+			&& authors[i].getLastName().equals(lName)) {
+				return authors[i];
+			}
+		}
+		return null;
+	}
+	
+	public Author addAndGetAuthor(Author author) {
+		Author foundAuthor = findAuthorByNames(author.getFirstName(), author.getLastName());
+		if(foundAuthor == null) {
+			addAuthor(author);
+			return author;
+		} else {
+			return foundAuthor;
+		}
+	}
+
 	public static String formatAsTable(Book[] books) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(String.format("|%3s|%30s|%16s|%4s|%10s|%10s|\n", "Id ", "Title              ", "Author      ", "Year",
@@ -89,13 +126,14 @@ public class Library {
 	public static void main(String[] args) {
 		Library library = new Library(); // Default constructor
 
-		Book b1 = new Book("Thinking in Java 4ed.", "Bruce Eckel", 2006);
-		library.addBook(b1);
-
-		Book b2 = new Book("Algorithms + Data Structures = Programs", "Niklaus Wirth", 1976, "ISBN 978-0-13-022418-7",
-				"Prentice-Hall", Genre.SOFTWARE,
-				new String[] { "programming", "algorithm", "data structure", "classic" });
-		library.addBook(b2);
+		Book b1 = new Book("Thinking in Java 4ed.", 
+				new Author[] {new Author("Bruce", "Eckel")}, 2006);
+		System.out.println(b1);
+		Book b2 = new Book("Algorithms + Data Structures = Programs", 
+				new Author[] {new Author("Niklaus", "Wirth")}, 1976, 
+				"ISBN 978-0-13-022418-7", "Prentice-Hall", Genre.SOFTWARE, 
+				new String[] {"programming", "algorithm", "data structure", "classic"});
+		System.out.println(b2);
 
 		// for(Book book : library.getAllBooks()) {
 		// System.out.println(book);
