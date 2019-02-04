24c4237ed6f9939291fd01a1e599f56d7013c089
Trayan Iliev <office@iproduct.org>
2019-02-04 19:15:06 +0200

    shortest path completed

--- java-problems/src/maze/ShortestPath.java
+++ java-problems/src/maze/ShortestPath.java
@@ -2,6 +2,8 @@ package maze;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ShortestPath {
@@ -49,13 +51,62 @@ public class ShortestPath {
 		}
 		return null;
 	}
+	
+	public List<Position> findShortestPath(Position start) {
+		Scanner sc = new Scanner(System.in);
+		LinkedList<List<Position>> queue = new LinkedList<>();
+		LinkedList<Position> initialPath = new LinkedList<>();
+		initialPath.add(start);
+		queue.offer(initialPath);
+		
+		while(!queue.isEmpty()) {
+			List<Position> nextPath = queue.pollFirst();
+			Position last = nextPath.get(nextPath.size() - 1);
+			int x = last.getX();
+			int y = last.getY();
+
+			if (x < 1 || x >= mazeSize-1 || y < 1 || y >= mazeSize-1) {
+				return nextPath;
+			}
+			if (maze[y][x - 1] == '0') {
+				maze[y][x - 1] =  (char)('0' + nextPath.size());
+				LinkedList<Position> newPath = new LinkedList<>(nextPath);
+				newPath.add(new Position(x-1, y));
+				queue.offer(newPath);
+			}
+			if (maze[y][x + 1] == '0') {
+				maze[y][x + 1] =  (char)('0' + nextPath.size());
+				LinkedList<Position> newPath = new LinkedList<>(nextPath);
+				newPath.add(new Position(x + 1, y));
+				queue.offer(newPath);
+			}
+			if (maze[y - 1][x] == '0') {
+				maze[y - 1][x] =  (char)('0' + nextPath.size());
+				LinkedList<Position> newPath = new LinkedList<>(nextPath);
+				newPath.add(new Position(x, y - 1));
+				queue.offer(newPath);
+			}
+			if (maze[y + 1][x] == '0') {
+				maze[y + 1][x] =  (char)('0' + nextPath.size());
+				LinkedList<Position> newPath = new LinkedList<>(nextPath);
+				newPath.add(new Position(x, y + 1));
+				queue.offer(newPath);
+			}
+			System.out.println(getMazeAsString());
+			sc.nextLine();
+		}
+		return null;
+	}
 
 	public static void main(String[] args) {
 		ShortestPath problem = new ShortestPath("maze.txt");
 		try {
 			problem.readMazeFile();
 			System.out.println(problem.getMazeAsString());
-			System.out.println("Curiosity position: "+ problem.getCuriosityPosition());
+			Position start = problem.getCuriosityPosition();
+			System.out.println("Curiosity position: "+ start);
+			System.out.println(problem.findShortestPath(start));
+			
 		} catch (InvalidMazeFileFormatException e) {
 			System.err.println(e.getMessage());
 		}
